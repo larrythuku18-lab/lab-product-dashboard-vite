caba2909553fc9b509ba1b17662f5426d7476527
Larry Thuku <larrythuku18@gmail.com>
2026-05-05 21:19:46 +0300

    Initialized a product card containing name, price and instock and also added a button that alerts an execution on whether or not removing a product or added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,14 +3,17 @@ import styles from '../styles/ProductCard.module.css';
 
 const ProductCard = ({ product }) => {
   return (
-    <div className>
-      {/* TODO: Apply conditional class to <div> above for out-of-stock items */}
+    <div className ={containerClass}>
       
       {/* TODO: Display product name */}
+      <h2>{product.name}</h2>
 
       {/* TODO: Display product price */}
+      <p>{product.price}</p>
 
       {/* TODO: Show if the product is in stock or out of stock */}
+      <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
+      <button onClick={() => alert(`Removing ${product.name}`)}>Remove</button>
       
     </div>
   );
