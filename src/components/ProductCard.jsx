0b6d377ee0610f8d49e18993ba78ec054de0cbd2
Larry Thuku <larrythuku18@gmail.com>
2026-05-05 21:43:58 +0300

    Error Handling feat/productlisst.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,21 +2,16 @@ import React from 'react';
 import styles from '../styles/ProductCard.module.css';
 
 const ProductCard = ({ product, onRemove }) => {
+  const containerClass = product.inStock
+    ? styles.productCard
+    : `${styles.productCard} ${styles.outOfStockClass}`;
 
-  const containerClass = product.inStock ? styles.inStock : styles.outOfStock;
   return (
-    <div className ={containerClass}>
-      
-      {/* TODO: Display product name */}
+    <div className={containerClass}>
       <h3>{product.name}</h3>
-
-      {/* TODO: Display product price */}
       <p>{product.price}</p>
-
-      {/* TODO: Show if the product is in stock or out of stock */}
       <p>{product.inStock ? "In Stock" : "Out of Stock"}</p>
       <button onClick={() => onRemove(product.id)}>Remove</button>
-      
     </div>
   );
 };
